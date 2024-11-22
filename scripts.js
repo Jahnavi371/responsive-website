aa6b8f9777a191e856004b653010d5bda1ceb025
Jahnavi371 <jonnawadajahnavi@gmail.com>
2024-11-22 16:52:53 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Example JavaScript for form validation
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent form submission for demo purposes
+    alert("Reservation submitted!");
+  });
+  
